de0e2231dfae7a05c063f4093d53d64321eab337
Naga Venkatesh <rnvenkateshs@outlook.com>
2020-08-26 23:49:06 +0530

    level 4 : ex8

---
+++ Exercises-Level4/ex8.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//create a map with slice values
+	m := map[string][]string{
+		`bond_james`:      []string{`Shaken, not stirred`, `Martinis`, `Women`},
+		`moneypenny_miss`: []string{`James Bond`, `Literature`, `Computer Science`},
+	}
+
+	// add a slice to the map
+	m["no_dr"] = []string{`Being evil`, `Ice cream`, `Sunsets`}
+
+	//loop through all map range and the slice values
+	for k, v := range m {
+		fmt.Println("Key : ", k)
+		for i, s := range v {
+			fmt.Printf("slice value at %d : %v\n", i, s)
+		}
+	}
+}
